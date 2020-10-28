dd28ce426f58f92f8f39b92ce56c2b661088ca68
radacraluca <71904496+radacraluca@users.noreply.github.com>
2020-10-28 23:09:50 +0200

    Add files via upload

---
+++ radacraluca/L05/Profile.Domain/CreateProfileWorkflow/InvalidEmailException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Tema5Radac.Profile.Domain.CreateProfileWorkflow
+{
+    [Serializable]
+    public class InvalidEmailException : Exception
+    {
+        public InvalidEmailException()
+        {
+        }
+
+        public InvalidEmailException(string email) : base($"Value  is an invalid e-mail format.")
+        {
+        }
+
+    }
+}
---
+++ radacraluca/L05/Profile.Domain/CreateProfileWorkflow/VoteEnum.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tema5Radac.Profile.Domain.CreateProfileWorkflow
+{
+    public enum VoteEnum
+    {
+        Up = 1,
+        Down = -1
+    }
+}
