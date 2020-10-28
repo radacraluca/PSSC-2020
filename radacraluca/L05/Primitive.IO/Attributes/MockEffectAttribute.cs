b40f01d4d4216e6c522e14c988150f30ebaf42d8
radacraluca <71904496+radacraluca@users.noreply.github.com>
2020-10-28 23:00:51 +0200

    Add files via upload

---
+++ radacraluca/L05/Primitive.IO/Attributes/MockEffectAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tema5Radac.Primitives.IO.Attributes
+{
+    public class MockEffectAttribute : Attribute
+    {
+        public object Effect { get; }
+
+        public MockEffectAttribute(object effect)
+        {
+            Effect = effect;
+        }
+    }
+}
