c5165c8028920633b995e330453c8654804ac6d0
radacraluca <71904496+radacraluca@users.noreply.github.com>
2020-11-11 22:12:08 +0200

    Add files via upload

---
+++ radacraluca/L06/Access.Primitives.IO/Outputs/SendAckToReplyAuthorResult.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tema6.Outputs
+{
+    public static partial class SendAckToReplyAuthorResult
+    {
+        public interface ISendAskToReplyAuthorResult { };
+        public class ReplyPublished : ISendAskToReplyAuthorResult
+        {
+            public string ConfirmationMessage { get; }
+            public ReplyPublished(string confirmationMessage)
+            {
+                ConfirmationMessage = confirmationMessage;
+            }
+        }
+        public class InvalidReplyPublished : ISendAskToReplyAuthorResult
+        {
+            public string ErrorMessage { get; }
+            public InvalidReplyPublished(string errormessage)
+            {
+                ErrorMessage = errormessage;
+            }
+        }
+    }
+}
