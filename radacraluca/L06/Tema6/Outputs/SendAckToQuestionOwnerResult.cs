e18a1a561f776e77cdaed5d28eccd3507dd529c2
radacraluca <71904496+radacraluca@users.noreply.github.com>
2020-11-11 22:38:05 +0200

    Add files via upload

---
+++ radacraluca/L06/Tema6/Outputs/SendAckToQuestionOwnerResult.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tema6.Outputs
+{
+    public static partial class SendAckToQuestionOwnerResult
+    {
+        public interface ISendAckToQuestionOwnerResult { };
+        public class ReplyReceived : ISendAckToQuestionOwnerResult
+        {
+            public String Text { get; }
+            public ReplyReceived(String text)
+            {
+                Text = text;
+            }
+        }
+        public class InvalidReplyReceived : ISendAckToQuestionOwnerResult
+        {
+            public String ErrorMessage { get; }
+            public InvalidReplyReceived(String errormessage)
+            {
+                ErrorMessage = errormessage;
+            }
+        }
+
+    }
+}
