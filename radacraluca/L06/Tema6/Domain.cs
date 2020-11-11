ea5971f646e675b6b246890cfe5f8dc014ecd752
radacraluca <71904496+radacraluca@users.noreply.github.com>
2020-11-11 22:27:59 +0200

    Add files via upload

---
+++ radacraluca/L06/Tema6/Domain.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Access.Primitives.IO;
+using Tema6.Inputs;
+using Tema6.Outputs;
+using static PortExt;
+
+namespace Tema6
+{
+    public static class BoundedContext
+    {
+        public static Port<ValidateReplyResult.IValidateReplyResult> ValidateReply(int authorId, int questionId,
+            string answer)
+            => NewPort<ValidateReplyCmd, ValidateReplyResult.IValidateReplyResult>(
+                new ValidateReplyCmd(authorId, questionId, answer));
+
+        public static Port<CheckLanguageResult.ICheckLanguageResult> CheckLanguage(string text)
+            => NewPort<CheckLanguageCmd, CheckLanguageResult.ICheckLanguageResult>(new CheckLanguageCmd(text));
+
+    }
+}
---
+++ radacraluca/L06/Tema6/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Tema6;
+using Tema6.Outputs;
+using Access.Primitives.IO;
+using Tema6.Adapters;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+using System.Security.Authentication.ExtendedProtection;
+using System.Threading.Tasks;
+using LanguageExt;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Tema6
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var wf = from createReplyResult in BoundedContext.ValidateReply(30, 1, "Deep Learning")
+                     let validReply = (ValidateReplyResult.ReplyValidated)createReplyResult
+                     from checkLanguageResult in BoundedContext.CheckLanguage(validReply.Reply.Answer)
+                     from ownerAck in BoundedContext.SendAckToQuestionOwner((CheckLanguageResult.TextChecked)checkLanguageResult)
+                     from authorAck in BoundedContext.SendAckToReplyAuthor((CheckLanguageResult.CheckFailed)checkLanguageResult)
+                     select (validReply, checkLanguageResult, ownerAck, authorAck);
+
+            var serviceProvider = new ServiceCollection()
+                .AddOperations(typeof(ValidateReplyAdapter).Assembly)
+                .AddOperations(typeof(CheckLanguageAdapter).Assembly)
+                .AddOperations(typeof(SenAckToQuestionOwnerAdapter).Assembly)
+                .AddTransient<IInterpreterAsync>(sp => new LiveInterpreterAsync(sp))
+                .BuildServiceProvider();
+            var interpreter = serviceProvider.GetService<IInterpreterAsync>();
+            var writeContext = new QuestionWriteContext(new List<int>() { 1, 2, 3 }, new List<int>() { 30, 31, 32 });
+            var result = await interpreter.Interpret(wf, writeContext);
+            Console.WriteLine("Hello World!");
+        }
+
+    }
+    internal interface IReplyPosted
+    {
+    }
+}
