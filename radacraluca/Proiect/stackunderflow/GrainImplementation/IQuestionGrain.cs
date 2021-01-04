6ec9a16f2c4938cddcb92bf37034c71412e622bc
radacraluca <71904496+radacraluca@users.noreply.github.com>
2021-01-05 00:32:40 +0200

    Add files via upload

---
+++ radacraluca/Proiect/stackunderflow/GrainImplementation/IQuestionGrain.cs
@@ -0,0 +1,14 @@
+﻿using Orleans;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using StackUnderflow.EF.Models;
+
+namespace GrainInterfaces
+{
+    public interface IQuestionnGrain : IGrainWithStringKey
+    {
+        Task<IEnumerable<Post>> GetQuestionsAsync();
+    }
+}
